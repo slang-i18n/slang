fd3c49a758274676c8035da82b23cee56652da3b
Tien Do Nam <dev.tien.donam@gmail.com>
2020-11-04 16:24:39 +0100

    test: update regex

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -2,60 +2,36 @@ import 'package:fast_i18n/utils.dart';
 import 'package:test/test.dart';
 
 void main() {
-  testSpecialRegex();
   testLocaleRegex();
   testNormalize();
-}
-
-void testSpecialRegex() {
-  group('specialRegex', () {
-    RegExp regex = Utils.specialRegex;
-
-    test('contains space', () {
-      expect(regex.hasMatch('hello '), true);
-    });
-
-    test('contains dot', () {
-      expect(regex.hasMatch('hello.'), true);
-    });
-
-    test('contains underscore', () {
-      expect(regex.hasMatch('hello_'), true);
-    });
-
-    test('contains dash', () {
-      expect(regex.hasMatch('hello-'), true);
-    });
-
-    test('contains no special character', () {
-      expect(regex.hasMatch('hello'), false);
-    });
-  });
 }
 
 void testLocaleRegex() {
   group('localeRegex', () {
     RegExp regex = Utils.localeRegex;
 
-    test('_en', () {
-      RegExpMatch match = regex.firstMatch('_en');
-      expect(match.group(1), 'en');
+    test('strings_en', () {
+      RegExpMatch match = regex.firstMatch('strings_en');
+      expect(match.group(2), 'strings'); // base name
+      expect(match.group(3), 'en'); // language
     });
 
-    test('_en_US', () {
-      RegExpMatch match = regex.firstMatch('_en_US');
-      expect(match.group(1), 'en');
-      expect(match.group(3), 'US');
+    test('strings_en_US', () {
+      RegExpMatch match = regex.firstMatch('strings_en_US');
+      expect(match.group(2), 'strings'); // base name
+      expect(match.group(3), 'en');
+      expect(match.group(5), 'US');
     });
 
-    test('_en-US', () {
-      RegExpMatch match = regex.firstMatch('_en-US');
-      expect(match.group(1), 'en');
-      expect(match.group(3), 'US');
+    test('translations_en-US', () {
+      RegExpMatch match = regex.firstMatch('translations_en-US');
+      expect(match.group(2), 'translations'); // base name
+      expect(match.group(3), 'en');
+      expect(match.group(5), 'US');
     });
 
-    test('_enUS', () {
-      RegExpMatch match = regex.firstMatch('_enUS');
+    test('strings_enUS', () {
+      RegExpMatch match = regex.firstMatch('strings_enUS');
       expect(match, null);
     });
   });
