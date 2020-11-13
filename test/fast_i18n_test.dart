00e3ca79cbbc071b0786b9c162879cea7a899482
Tien Do Nam <dev.tien.donam@gmail.com>
2020-11-13 18:28:45 +0100

    test: adapt to changes

--- test/fast_i18n_test.dart
+++ test/fast_i18n_test.dart
@@ -8,27 +8,27 @@ void main() {
 void testSelectLocale() {
   group('selectLocale', () {
     test('match exactly', () {
-      expect(FastI18n.selectLocale('en-us', ['en-us', 'de-de']), 'en-us');
+      expect(FastI18n.selectLocale('en-us', ['en-us', 'de-de'], ''), 'en-us');
     });
 
     test('match exactly with no country code', () {
-      expect(FastI18n.selectLocale('de', ['en', 'de']), 'de');
+      expect(FastI18n.selectLocale('de', ['en', 'de'], ''), 'de');
     });
 
     test('match exactly but need normalizing', () {
-      expect(FastI18n.selectLocale('en_US', ['en-us', 'de-de']), 'en-us');
+      expect(FastI18n.selectLocale('en_US', ['en-us', 'de-de'], ''), 'en-us');
     });
 
     test('match first part', () {
-      expect(FastI18n.selectLocale('de_DE', ['en', 'de']), 'de');
+      expect(FastI18n.selectLocale('de_DE', ['en', 'de'], ''), 'de');
     });
 
     test('prefer first part over second part', () {
-      expect(FastI18n.selectLocale('en_DE', ['de', 'en']), 'en');
+      expect(FastI18n.selectLocale('en_DE', ['de', 'en'], ''), 'en');
     });
 
     test('match last part', () {
-      expect(FastI18n.selectLocale('en_US', ['us', 'de']), 'us');
+      expect(FastI18n.selectLocale('en_US', ['us', 'de'], ''), 'us');
     });
 
     test('fallback', () {
