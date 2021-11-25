c3700dfea99b5d493f506b3aabe4e6234adc7904
Tien Do Nam <dev.tien.donam@gmail.com>
2021-11-25 01:15:20 +0100

    refactor: remove yaml_parse_result.dart

--- lib/src/model/yaml_parse_result.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fast_i18n/src/model/build_config.dart';
-
-/// result of the build.yaml parser
-class YamlParseResult {
-  final bool parsed;
-  final BuildConfig config;
-
-  YamlParseResult({required this.parsed, required this.config});
-}
