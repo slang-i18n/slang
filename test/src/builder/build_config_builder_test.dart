50037ce77783cab78cebcd0b0d9fcf057c099782
Tien Do Nam <dev.tien.donam@gmail.com>
2021-11-28 21:00:08 +0100

    feat: add build config test

--- test/src/builder/build_config_builder_test.dart
+++ test/src/builder/build_config_builder_test.dart
@@ -2,6 +2,32 @@ import 'package:fast_i18n/src/builder/build_config_builder.dart';
 import 'package:test/test.dart';
 
 void main() {
+  group(BuildConfigBuilder.fromYaml, () {
+    test('context gender default', () {
+      final result = BuildConfigBuilder.fromYaml(r'''
+        targets:
+          $default:
+            builders:
+              fast_i18n:
+                options:
+                  input_directory: lib/i18n
+                  fallback_strategy: base_locale
+                  contexts:
+                    GenderContext:
+                      enum:
+                        - male
+                        - female
+        ''');
+
+      expect(result, isNotNull);
+      expect(result!.contexts.length, 1);
+      expect(result.contexts.first.enumName, 'GenderContext');
+      expect(result.contexts.first.enumValues, ['male', 'female']);
+      expect(result.contexts.first.auto, true);
+      expect(result.contexts.first.paths, []);
+    });
+  });
+
   group(BuildConfigBuilder.fromMap, () {
     test('context gender default', () {
       final result = BuildConfigBuilder.fromMap(
