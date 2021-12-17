13674b2242737201466a7b4df3282cfb3a088712
Tien Do Nam <dev.tien.donam@gmail.com>
2021-12-18 00:19:00 +0100

    style: format

--- lib/src/generator/helper.dart
+++ lib/src/generator/helper.dart
@@ -2,18 +2,22 @@ import 'package:fast_i18n/src/model/build_config.dart';
 import 'package:fast_i18n/src/model/i18n_locale.dart';
 import 'package:fast_i18n/src/utils/string_extensions.dart';
 
-String getClassNameRoot(
-    {required String baseName,
-    String locale = '',
-    required TranslationClassVisibility visibility}) {
+String getClassNameRoot({
+  required String baseName,
+  String locale = '',
+  required TranslationClassVisibility visibility,
+}) {
   String result = baseName.toCase(CaseStyle.pascal) +
       locale.toLowerCase().toCase(CaseStyle.pascal);
   if (visibility == TranslationClassVisibility.private) result = '_' + result;
   return result;
 }
 
-String getClassName(
-    {required String parentName, String childName = '', I18nLocale? locale}) {
+String getClassName({
+  required String parentName,
+  String childName = '',
+  I18nLocale? locale,
+}) {
   return parentName +
       childName.toCase(CaseStyle.pascal) +
       (locale != null
