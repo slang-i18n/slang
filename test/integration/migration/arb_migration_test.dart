f3e236cce86bd6b169c10909e7055f27f6bd378e
fzyzcjy <ch271828n@outlook.com>
2022-04-29 10:02:13 +0800

    fix compile error

--- test/integration/migration/arb_migration_test.dart
+++ test/integration/migration/arb_migration_test.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:fast_i18n/src/tools/migrate_arb.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import '../../util/resources_utils.dart';
 
