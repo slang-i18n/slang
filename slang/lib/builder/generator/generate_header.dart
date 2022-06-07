d290794bfcf6d3ab064e7146fe6eb98684b76cbe
fzyzcjy <ch271828n@outlook.com>
2022-06-08 07:50:32 +0800

    add `unnecessary_string_interpolations`

--- slang/lib/builder/generator/generate_header.dart
+++ slang/lib/builder/generator/generate_header.dart
@@ -111,7 +111,7 @@ void _generateHeaderComment({
 
   buffer.writeln();
   buffer.writeln(
-      '// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, library_private_types_in_public_api');
+      '// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, library_private_types_in_public_api, unnecessary_string_interpolations');
 }
 
 void _generateImports(I18nConfig config, StringBuffer buffer) {
