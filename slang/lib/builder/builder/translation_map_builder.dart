7218acefe11ba00a47b54d336c02608f8beee369
Tien Do Nam <dev.tien.donam@gmail.com>
2022-11-12 18:27:42 +0100

    feat: improve pad left debug string

--- slang/lib/builder/builder/translation_map_builder.dart
+++ slang/lib/builder/builder/translation_map_builder.dart
@@ -8,9 +8,6 @@ import 'package:slang/builder/model/translation_map.dart';
 import 'package:slang/builder/utils/path_utils.dart';
 import 'package:slang/builder/utils/regex_utils.dart';
 
-const _defaultPadLeft = 12;
-const _namespacePadLeft = 24;
-
 class TranslationMapBuilder {
   /// Read all files and build a [TranslationMap]
   /// containing all locales, their namespaces and their locales
@@ -20,7 +17,14 @@ class TranslationMapBuilder {
     required bool verbose,
   }) async {
     final translationMap = TranslationMap();
-    final padLeft = rawConfig.namespaces ? _namespacePadLeft : _defaultPadLeft;
+    final padLeft = verbose
+        ? _getPadLeft(
+            files: files,
+            baseLocale: rawConfig.baseLocale.languageTag,
+            namespaces: rawConfig.namespaces,
+            inputDirectory: rawConfig.inputDirectory,
+          )
+        : 0;
     for (final file in files) {
       final content = await file.read();
       final Map<String, dynamic> translations;
@@ -57,10 +61,10 @@ class TranslationMapBuilder {
             );
 
             if (verbose) {
-              final namespaceLog = rawConfig.namespaces ? '($namespace) ' : '';
-              final base = locale == rawConfig.baseLocale ? '(base) ' : '';
+              final baseStr = locale == rawConfig.baseLocale ? '(base) ' : '';
+              final namespaceStr = rawConfig.namespaces ? '($namespace) ' : '';
               print(
-                  '${('$base$namespaceLog${locale.languageTag}').padLeft(padLeft)} -> ${file.path}');
+                  '${('$baseStr$namespaceStr${locale.languageTag}').padLeft(padLeft)} -> ${file.path}');
             }
           });
         } else {
@@ -109,9 +113,10 @@ class TranslationMapBuilder {
           );
 
           if (verbose) {
-            final namespaceLog = rawConfig.namespaces ? '($namespace) ' : '';
+            final baseStr = locale == rawConfig.baseLocale ? '(base) ' : '';
+            final namespaceStr = rawConfig.namespaces ? '($namespace) ' : '';
             print(
-                '${(namespaceLog + locale.languageTag).padLeft(padLeft)} -> ${file.path}');
+                '${(baseStr + namespaceStr + locale.languageTag).padLeft(padLeft)} -> ${file.path}');
           }
         }
       }
@@ -127,5 +132,74 @@ class TranslationMapBuilder {
     }
 
     return translationMap;
+  }
+}
+
+const _BASE_STR_LENGTH = 7; // "(base) "
+
+/// Determines the longest debug string used for PadLeft
+int _getPadLeft(
+    {required List<TranslationFile> files,
+    required String baseLocale,
+    required bool namespaces,
+    required String? inputDirectory}) {
+  int longest = 0;
+  for (final file in files) {
+    int currLength;
+    final fileNameNoExtension = PathUtils.getFileNameNoExtension(file.path);
+    final baseFileMatch =
+        RegexUtils.baseFileRegex.firstMatch(fileNameNoExtension);
+
+    if (baseFileMatch != null) {
+      final namespace = baseFileMatch.group(1)!;
+      I18nLocale? directoryLocale = null;
+      if (namespaces) {
+        directoryLocale = PathUtils.findDirectoryLocale(
+          filePath: file.path,
+          inputDirectory: inputDirectory,
+        );
+        currLength = namespace.length +
+            (directoryLocale?.languageTag ?? baseLocale).length;
+      } else {
+        currLength = baseLocale.length;
+      }
+      if ((directoryLocale?.languageTag ?? baseLocale) == baseLocale) {
+        currLength += _BASE_STR_LENGTH;
+      }
+    } else {
+      final match =
+          RegexUtils.fileWithLocaleRegex.firstMatch(fileNameNoExtension);
+      if (match != null) {
+        final namespace = match.group(1)!;
+        final locale = I18nLocale(
+          language: match.group(2)!,
+          script: match.group(3),
+          country: match.group(4),
+        );
+
+        if (namespaces) {
+          currLength = namespace.length + locale.languageTag.length;
+        } else {
+          currLength = locale.languageTag.length;
+        }
+        if (locale.languageTag == baseLocale) {
+          currLength += _BASE_STR_LENGTH;
+        }
+      } else {
+        currLength = 0;
+      }
+    }
+
+    if (currLength > longest) {
+      longest = currLength;
+    }
+  }
+
+  if (namespaces) {
+    // (base) (namespace) locale
+    return longest + 4; // add first space, '(', ')', and last space
+  } else {
+    // (base) locale
+    return longest + 1; // only add first space
   }
 }
