4b035f0e9d29240a06a2cf133e98ef9e07aa0e5e
Tien Do Nam <dev.tien.donam@gmail.com>
2024-08-24 17:35:21 +0200

    refactor: make string to enum parsers internal

--- slang/lib/builder/builder/raw_config_builder.dart
+++ slang/lib/builder/builder/raw_config_builder.dart
@@ -225,4 +225,78 @@ extension on String {
   String removeTrailingSlash() {
     return endsWith('/') ? substring(0, length - 1) : this;
   }
+
+  FallbackStrategy? toFallbackStrategy() {
+    switch (this) {
+      case 'none':
+        return FallbackStrategy.none;
+      case 'base_locale':
+        return FallbackStrategy.baseLocale;
+      case 'base_locale_empty_string':
+        return FallbackStrategy.baseLocaleEmptyString;
+      default:
+        return null;
+    }
+  }
+
+  OutputFormat? toOutputFormat() {
+    switch (this) {
+      case 'single_file':
+        return OutputFormat.singleFile;
+      case 'multiple_files':
+        return OutputFormat.multipleFiles;
+      default:
+        return null;
+    }
+  }
+
+  TranslationClassVisibility? toTranslationClassVisibility() {
+    switch (this) {
+      case 'private':
+        return TranslationClassVisibility.private;
+      case 'public':
+        return TranslationClassVisibility.public;
+      default:
+        return null;
+    }
+  }
+
+  StringInterpolation? toStringInterpolation() {
+    switch (this) {
+      case 'dart':
+        return StringInterpolation.dart;
+      case 'braces':
+        return StringInterpolation.braces;
+      case 'double_braces':
+        return StringInterpolation.doubleBraces;
+      default:
+        return null;
+    }
+  }
+
+  CaseStyle? toCaseStyle() {
+    switch (this) {
+      case 'camel':
+        return CaseStyle.camel;
+      case 'snake':
+        return CaseStyle.snake;
+      case 'pascal':
+        return CaseStyle.pascal;
+      default:
+        return null;
+    }
+  }
+
+  PluralAuto? toPluralAuto() {
+    switch (this) {
+      case 'off':
+        return PluralAuto.off;
+      case 'cardinal':
+        return PluralAuto.cardinal;
+      case 'ordinal':
+        return PluralAuto.ordinal;
+      default:
+        return null;
+    }
+  }
 }
