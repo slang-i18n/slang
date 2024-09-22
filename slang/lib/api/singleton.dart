645a2554c718c4f81f3c4d377d614f0f8d9d2df4
Piotr Orzechowski <piotr@orzechowski.tech>
2024-09-22 07:15:27 +0200

    Update Unicode CLDR links (#246)

--- slang/lib/api/singleton.dart
+++ slang/lib/api/singleton.dart
@@ -271,7 +271,7 @@ extension LocaleSettingsExt<E extends BaseAppLocale<E, T>,
   }
 
   /// Sets plural resolvers.
-  /// See https://unicode-org.github.io/cldr-staging/charts/latest/supplemental/language_plural_rules.html
+  /// See https://www.unicode.org/cldr/charts/latest/supplemental/language_plural_rules.html
   /// See https://github.com/slang-i18n/slang/blob/main/slang/lib/api/plural_resolver_map.dart
   /// Either specify [language], or [locale]. [locale] has precedence.
   void setPluralResolver({
