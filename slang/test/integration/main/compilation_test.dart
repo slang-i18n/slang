1c7c5df0801ef578d6650f61392102e42e3fbff6
Tien Do Nam <dev.tien.donam@gmail.com>
2024-12-07 03:41:34 +0100

    test: enable compilation test

--- slang/test/integration/main/compilation_test.dart
+++ slang/test/integration/main/compilation_test.dart
@@ -1,28 +1,18 @@
-@Skip('not updated for multiple files')
-library;
-
-import 'package:expect_error/expect_error.dart';
+import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:test/test.dart';
 
 import '../../util/resources_utils.dart';
 
 /// These tests ensure that the generated code compiles.
+/// It currently only checks for syntax errors.
 void main() {
-  late Library library;
-
-  setUp(() async {
-    // A workaround so we have Flutter available in the analyzer.
-    // See: https://pub.dev/packages/expect_error#flutter-support
-    library = await Library.custom(
-      packageName: 'slang_flutter',
-      path: 'not used',
-      packageRoot: '../slang_flutter',
-    );
-  });
-
-  Future<void> expectCompiles(String path) {
+  void expectCompiles(String path) {
     final output = loadResource(path);
-    return expectLater(library.withCode(output), compiles);
+    final result = parseString(
+      path: 'path.dart',
+      content: output,
+    );
+    expect(result.errors, isEmpty);
   }
 
   test('fallback base locale', () {
@@ -39,10 +29,6 @@ void main() {
 
   test('rich text', () {
     expectCompiles('main/_expected_rich_text.output');
-  });
-
-  test('single output', () {
-    expectCompiles('main/_expected_single.output');
   });
 
   test('translation overrides', () {
